f8be5bba0d77972e01238f447c9d6892fc92093e
masihtamsoy <masihtamsoy@gmail.com>
2017-11-19 16:48:27 +0530

    Toggle Chatbox when clicked on floating bubble

--- src/containers/DashboardPage.js
+++ src/containers/DashboardPage.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {cyan600, pink600, purple600, orange600} from 'material-ui/styles/colors';
+import React, { Component } from 'react';
+import { cyan600, pink600, purple600, orange600 } from 'material-ui/styles/colors';
 import Assessment from 'material-ui/svg-icons/action/assessment';
 import Face from 'material-ui/svg-icons/action/face';
 import ThumbUp from 'material-ui/svg-icons/action/thumb-up';
@@ -14,82 +14,103 @@ import Data from '../data';
 
 import ChatBot from 'react-simple-chatbot';
 
-const DashboardPage = () => {
 
-  return (
-    <div>
-      <h3 style={globalStyles.navigation}>Application / Dashboard</h3>
+class DashboardPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chatbot: {
+        opened: false
+      }
+    };
+  }
 
-      <div className="row">
+  _toggleChatbotFloating = () => {
+    let prevOpenedState = this.state.chatbot.opened;
+    this.setState({chatbot: {opened: !prevOpenedState}})
+  }
 
-        <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 ">
-          <InfoBox Icon={ShoppingCart}
-                   color={pink600}
-                   title="Total Profit"
-                   value="1500k"
-          />
+  render() {
+    const {
+      chatbot
+    } = this.state;
+
+    return (
+      <div>
+        <h3 style={globalStyles.navigation}>Application / Dashboard</h3>
+
+        <div className="row">
+
+          <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 ">
+            <InfoBox Icon={ShoppingCart}
+              color={pink600}
+              title="Total Profit"
+              value="1500k"
+            />
+          </div>
+
+
+          <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 ">
+            <InfoBox Icon={ThumbUp}
+              color={cyan600}
+              title="Likes Here"
+              value="4231"
+            />
+          </div>
+
+          <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 ">
+            <InfoBox Icon={Assessment}
+              color={purple600}
+              title="Sales"
+              value="460"
+            />
+          </div>
+
+          <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 ">
+            <InfoBox Icon={Face}
+              color={orange600}
+              title="New Members"
+              value="248"
+            />
+          </div>
         </div>
 
+        <div className="row">
+          <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-md m-b-15">
+            <NewOrders data={Data.dashBoardPage.newOrders} />
+          </div>
 
-        <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 ">
-          <InfoBox Icon={ThumbUp}
-                   color={cyan600}
-                   title="Likes Here"
-                   value="4231"
-          />
+          <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 m-b-15">
+            <MonthlySales data={Data.dashBoardPage.monthlySales} />
+          </div>
         </div>
 
-        <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 ">
-          <InfoBox Icon={Assessment}
-                   color={purple600}
-                   title="Sales"
-                   value="460"
-          />
-        </div>
+        <div className="row">
+          <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 m-b-15 ">
+            <RecentlyProducts data={Data.dashBoardPage.recentProducts} />
+          </div>
 
-        <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 ">
-          <InfoBox Icon={Face}
-                   color={orange600}
-                   title="New Members"
-                   value="248"
-          />
+          <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 m-b-15 ">
+            <BrowserUsage data={Data.dashBoardPage.browserUsage} />
+          </div>
         </div>
+        <ChatBot
+          steps={[
+            {
+              id: 'hello-world',
+              message: 'Hello World!',
+              end: true,
+            },
+          ]}
+          opened={chatbot.opened}
+          className={"rsc-float-button"}
+          floating={true}
+          toggleFloating={this._toggleChatbotFloating}
+        />
+
       </div>
-
-      <div className="row">
-        <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-md m-b-15">
-          <NewOrders data={Data.dashBoardPage.newOrders}/>
-        </div>
-
-        <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 m-b-15">
-          <MonthlySales data={Data.dashBoardPage.monthlySales}/>
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 m-b-15 ">
-          <RecentlyProducts data={Data.dashBoardPage.recentProducts}/>
-        </div>
-
-        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 m-b-15 ">
-          <BrowserUsage data={Data.dashBoardPage.browserUsage}/>
-        </div>
-      </div>
-      <ChatBot
-        steps={[
-          {
-            id: 'hello-world',
-            message: 'Hello World!',
-            end: true,
-          },
-        ]}
-        opened={false}
-        className={"rsc-float-button"}
-        floating={true}
-      />
-
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default DashboardPage;
